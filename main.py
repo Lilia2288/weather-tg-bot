ae9427916f2891247dc340e665b54e504ab78d7d
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2025-01-10 19:42:29 +0200

    update

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import requests
 
+
 OWM_API_KEY = "38ca48c0966459cb26384fd8e3b549c4"
 TELEGRAM_TOKEN = "8110024487:AAFVbx6oqPfF4NELDU4vWHnp9K7Nb24-WEQ"
+
 
 def get_weather(city: str):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=uk&appid={OWM_API_KEY}"
@@ -17,13 +19,16 @@ def get_weather(city: str):
     else:
         return "Не вдалося знайти інформацію про погоду. Перевірте назву міста."
 
+# Обробка команди /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Створення клавіатури
     keyboard = [
         [KeyboardButton("Передати геопозицію", request_location=True)],
         [KeyboardButton("Ввести місто вручну")]
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-    await update.message.reply_text("Привіт! Виберіть опцію:", reply_markup=reply_markup)
+    await update.message.reply_text("Привіт! Оберіть одну з опцій:", reply_markup=reply_markup)
+
 
 async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     location = update.message.location
@@ -40,17 +45,21 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("Не вдалося отримати погоду за вашою геопозицією.")
 
+
 async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     city = update.message.text
     weather = get_weather(city)
     await update.message.reply_text(weather)
 
+
 def main():
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.LOCATION, handle_location))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+
 
     application.run_polling()
 
